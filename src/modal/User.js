e85952138c23ea85936acc150ef7b2f574afcb66
thanhltde <149461786+Liamm-shkerpear@users.noreply.github.com>
2025-05-29 09:54:23 +0700

    test-merge

--- src/modal/User.js
+++ src/modal/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: function() { return !this.socialLogin; }, unique: true },
+  username: { type: String, required: function() { return !this.socialLogin; }, unique: true }, //thanhabc123
   password: { type: String, required: function() { return !this.socialLogin; } },
   role: { type: String, enum: ['user', 'admin'], required: true, default: 'user' },
   email: { type: String, required: true, unique: true },
