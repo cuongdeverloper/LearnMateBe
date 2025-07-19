bfc11439f70af4d8de247bdb84c25e608db596f5
cuongdeverloper <136320623+cuongdeverloper@users.noreply.github.com>
2025-07-19 15:06:26 +0700

    306

--- src/server.js
+++ src/server.js
@@ -19,7 +19,7 @@ const server = http.createServer(app);
 
 const io = socketIo(server, {
   cors: {
-    origin: "process.env.CLIENT_URL",
+    origin: process.env.CLIENT_URL,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
   }
